92e2168bd6904c4a22fe8a15e709a089be190866
riyagoyal666 <98308048+riyagoyal666@users.noreply.github.com>
2023-08-03 10:52:50 +0530

    code1.cpp

--- code1.cpp
+++ code1.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    int t1 = 0, t2 = 1, nextTerm = 0, n;
+    int t1 = 0, t2 = 1, nTerm = 0, n;
 
     cout << "Enter a positive number: ";
     cin >> n;
@@ -10,13 +10,13 @@ int main() {
 
     cout << "Fibonacci Series: " << t1 << ", " << t2 << ", ";
 
-    nextTerm = t1 + t2;
+    nTerm = t1 + t2;
 
-    while(nextTerm <= n) {
-        cout << nextTerm << ", ";
+    while(nTerm <= n) {
+        cout << nTerm << ", ";
         t1 = t2;
         t2 = nextTerm;
-        nextTerm = t1 + t2;
+        nTerm = t1 + t2;
     }
     return 0;
 }
